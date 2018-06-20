c3ef8b4f6780bf694c4a0a8fba77fb2eb97c7cb6
Sarvesh Parab <sarveshsparab@gmail.com>
2018-06-20 12:38:03 +0530

    implementing theme changing with jquery

---
+++ js/scripts/themeChanger.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    /* Theme changing logic*/
+    $("#blackThemeId").click(function() {
+        $("#themeCSSID").attr("href","css/colorThemes/blackTheme.css");
+    });
+    $("#blueThemeId").click(function() {
+        $("#themeCSSID").attr("href","css/colorThemes/blueTheme.css");
+    });
+    $("#defaultThemeId").click(function() {
+        $("#themeCSSID").attr("href","css/colorThemes/defaultTheme.css");
+    });
+    $("#greenThemeId").click(function() {
+        $("#themeCSSID").attr("href","css/colorThemes/greenTheme.css");
+    });
+    $("#orangeThemeId").click(function() {
+        $("#themeCSSID").attr("href","css/colorThemes/orangeTheme.css");
+    });
+    $("#pinkThemeId").click(function() {
+        $("#themeCSSID").attr("href","css/colorThemes/pinkTheme.css");
+    });
+    $("#purpleThemeId").click(function() {
+        $("#themeCSSID").attr("href","css/colorThemes/purpleTheme.css");
+    });
+    $("#redThemeId").click(function() {
+        $("#themeCSSID").attr("href","css/colorThemes/redTheme.css");
+    });
+    $("#yellowThemeId").click(function() {
+        $("#themeCSSID").attr("href","css/colorThemes/yellowTheme.css");
+    });
+});
