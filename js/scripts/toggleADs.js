ec8cb052ca4b61fc8a18aa44e9c282ea902b31c3
Sarvesh Parab <sarveshsparab@gmail.com>
2018-06-20 22:26:21 +0530

    removing redundant code

--- js/scripts/toggleADs.js
+++ js/scripts/toggleADs.js
@@ -21,23 +21,4 @@ function showGratitude() {
 }
 function showWarning() {
     alert("ADs help run this website, PLEASE keep them on");
-    /*if( !$('#sp-form-modal-overlay').length ) {
-        $('body').append('<div id="sp-form-modal-overlay" class="sp-form-modal-overlay"></div>');
-    }
-
-    $('#sp-form-modal-overlay').on('click', function() {
-        $('#sp-form-modal-overlay').fadeOut();
-        $('.sp-form-modal').fadeOut();
-    });
-
-    form = $($(this).attr('href'));
-    $('#sp-form-modal-overlay').fadeIn();
-    form.css('top', '50%').css('left', '50%').css('margin-top', -form.outerHeight()/2).css('margin-left', -form.outerWidth()/2).fadeIn();
-    return false;
-
-    $('.modal-closer').on('click', function() {
-        $('#sp-form-modal-overlay').fadeOut();
-        $('.sp-form-modal').fadeOut();
-        return false;
-    });*/
 }
