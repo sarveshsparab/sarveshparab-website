b7fe942123b4df23e2de469d2960838d0e3359a7
Sarvesh Parab <sarveshsparab@gmail.com>
2018-06-20 16:31:01 +0530

    adding alert based warnings

--- js/scripts/toggleADs.js
+++ js/scripts/toggleADs.js
@@ -2,11 +2,13 @@ $(document).ready(function() {
     /* Google ADs toggle logic*/
     $("#adsToggleID").change(function() {
         if($(this).is(":checked")) {
+            showGratitude();
             $("#mainContentDivId").removeClass("mainContentWithoutADs");
             $("#adsContentDivId").removeClass("googleAdsDisabled");
             $("#mainContentDivId").addClass("mainContentWithADs");
             $("#adsContentDivId").addClass("googleAdsEnabled");
         } else {
+            showWarning();
             $("#mainContentDivId").removeClass("mainContentWithADs");
             $("#adsContentDivId").removeClass("googleAdsEnabled");
             $("#mainContentDivId").addClass("mainContentWithoutADs");
@@ -14,3 +16,28 @@ $(document).ready(function() {
         }
     });
 });
+function showGratitude() {
+    alert("Thank you for supporting ADs on this page");
+}
+function showWarning() {
+    alert("ADs help run this website, PLEASE keep them on");
+    /*if( !$('#sp-form-modal-overlay').length ) {
+        $('body').append('<div id="sp-form-modal-overlay" class="sp-form-modal-overlay"></div>');
+    }
+
+    $('#sp-form-modal-overlay').on('click', function() {
+        $('#sp-form-modal-overlay').fadeOut();
+        $('.sp-form-modal').fadeOut();
+    });
+
+    form = $($(this).attr('href'));
+    $('#sp-form-modal-overlay').fadeIn();
+    form.css('top', '50%').css('left', '50%').css('margin-top', -form.outerHeight()/2).css('margin-left', -form.outerWidth()/2).fadeIn();
+    return false;
+
+    $('.modal-closer').on('click', function() {
+        $('#sp-form-modal-overlay').fadeOut();
+        $('.sp-form-modal').fadeOut();
+        return false;
+    });*/
+}
