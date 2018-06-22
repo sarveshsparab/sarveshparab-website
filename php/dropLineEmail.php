4bddf3c41ba831ee22530821d8de015da623883e
Sarvesh Parab <sarveshsparab@gmail.com>
2018-06-22 13:12:00 +0530

    email sending PHP code

---
+++ php/dropLineEmail.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+if (isset($_POST['name']) && strtoupper($_POST['captcha']) == $_SESSION['captcha_id']) {
+    $to = 'sarveshsparab@gmail.com';
+    $subject = 'Message from Website | ' . $_POST['subject'];
+    $headers = 'From: ' . $_POST['email'] . "\r\n" . 'Reply-To: ' . $_POST['email'];
+
+    $message = 'Name: ' . $_POST['name'] . "\n" .
+        'E-mail: ' . $_POST['email'] . "\n" .
+        'Subject: ' . $_POST['subject'] . "\n" .
+        'Message: ' . $_POST['message'];
+
+    mail($to, $subject, $message, $headers);
+    if ($_POST['copy'] == 'on') {
+        mail($_POST['email'], $subject, $message, $headers);
+    }
+}
+?>
