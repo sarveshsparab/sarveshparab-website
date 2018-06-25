7ed2dfbf21c5447dcd1aad3ecedfaad429bd24d5
Sarvesh Parab <sarveshsparab@gmail.com>
2018-06-26 03:24:59 +0530

    words population abstracted

--- js/scripts/canvasResponsive.js
+++ js/scripts/canvasResponsive.js
@@ -1,6 +1,7 @@
 /* To Begin rendering the canvas */
 window.onload = function() {
     try {
+        popWords();
         TagCanvas.Start('mySkillsCloudCanvas','',{
             outlineMethod: 'none',
             noSelect: true,
@@ -28,3 +29,8 @@ $(document).ready(function () {
         resize();
     });
 });
+
+/* Cloud words/tags population */
+function popWords() {
+    $("#mySkillsCloudCanvas").html('<a>Google</a><a>Fish</a><a>Chips</a><a>Salt</a><a>Vinegar</a><a>Sugar</a><a>Pepper</a><a>Choco</a><a>Ginger</a><a>Garlic</a><a>Peanuts</a><a>Cashew</a><a>Almonds</a>');
+}
