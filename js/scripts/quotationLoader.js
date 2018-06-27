ac5007457c17641b09cc21181830e526a52afdf7
Sarvesh Parab <sarveshsparab@gmail.com>
2018-06-27 17:35:09 +0530

    quotations fetching code

--- js/scripts/quotationLoader.js
+++ js/scripts/quotationLoader.js
@@ -1,11 +1,16 @@
 $(function () {
     $.ajax({
-        url: "../../php/fetchQuotation.php",
+        url: "../../php/content/fetchQuotation.php",
         success: function (result) {
-            $("#topQuoteHomeDiv").html(result);
+            if(result == 'ERROR'){
+                console.error("Error : No rows returned from the quotations table | Result : "+result);
+                $("#topQuoteHomeDiv").html('<blockquote><p>Look up at the stars and not down at your feet. Try to make sense of what you see, and wonder about what makes the universe exist. Be curious. ”</p><footer>- Stephen Hawking</footer></blockquote>');
+            }else {
+                $("#topQuoteHomeDiv").html(result);
+            }
         },
         error: function () {
-            console.log("Error : In fetching quotation from DB");
+            console.error("Error : In AJAX call for fetching quotation");
             $("#topQuoteHomeDiv").html('<blockquote><p>Look up at the stars and not down at your feet. Try to make sense of what you see, and wonder about what makes the universe exist. Be curious. ”</p><footer>- Stephen Hawking</footer></blockquote>');
         }
     });
--- php/content/fetchQuotation.php
+++ php/content/fetchQuotation.php
@@ -6,20 +6,17 @@
  * Time: 15:09
  */
 
-include("db/connectGoDaddyDB.php");
+include("../db/connectGoDaddyDB.php");
 
 $sql = "SELECT q.id AS quote_id, q.content AS content, q.author AS author FROM quotations q ORDER BY RAND() LIMIT 1";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    // output data of each row
     while($row = $result->fetch_assoc()) {
         echo '<blockquote><p>'.$row["content"].'”</p><footer>- '.$row["author"].'</footer></blockquote>';
     }
 } else {
-    echo "0 results";
+    echo "ERROR";
 }
-
-$conn->close();
 
 ?>
