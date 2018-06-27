da2f4b17a1daad192d87949ba27b7edc0d48101f
Sarvesh Parab <sarveshsparab@gmail.com>
2018-06-28 04:09:07 +0530

    experience fetching from db

---
+++ php/content/fetchExperience.php
@@ -0,0 +1,42 @@
+<?php
+
+include("../db/connectGoDaddyDB.php");
+include("../util/utility.php");
+
+$sql = "SELECT * from experience exp ORDER BY exp.display_order DESC";
+$result = $conn->query($sql);
+
+$content = '';
+$content .= '<section class="cd-timeline js-cd-timeline"><div class="cd-timeline__container">';
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $content .= '<div class="cd-timeline__block js-cd-block"><div class="cd-timeline__sym js-cd-img"><span>';
+        $content .= $row["symbol"];
+        $content .= '</span></div><div class="cd-timeline__content js-cd-content"><h2>';
+        $content .= $row["designation"];
+        $content .= '</h2>';
+        if($row["short_desc"]!=null){
+            $content .= '<p>';
+            foreach(handleDesc($row["short_desc"]) as $descLine){
+                $content .= $descLine.'<br>';
+            }
+            $content .= '</p>';
+        }
+        if($row["long_desc"]!=null){
+            $content .= '<a id="edu_'.$row["id"].'" class="cd-timeline__read-more">Read more</a>';
+        }
+        $content .= '<span class="cd-timeline__name">';
+        $content .= $row["organisation"];
+        $content .= '</span><br><span class="cd-timeline__date">';
+        $content .= $row["time_frame"];
+        $content .= '</span></div></div>';
+    }
+    $content .= '</div></section>';
+} else {
+    $content = 'ERROR';
+}
+
+echo $content;
+
+?>
