b6317b43803b03cc731aa95b0bf9c19bc07385dd
Sarvesh Parab <sarveshsparab@gmail.com>
2018-06-27 19:56:01 +0530

    added jquery loader for ajax call

--- js/scripts/quotationLoader.js
+++ js/scripts/quotationLoader.js
@@ -2,6 +2,7 @@ $(function () {
     $.ajax({
         url: "../../php/content/fetchQuotation.php",
         success: function (result) {
+            $("#topQuoteHomeDiv").html('<div id="jqueryLoaderId" class="jqueryLoader" style="line-height: unset"><i class="fa fa-spin fa-spinner"></i></div>');
             if(result == 'ERROR'){
                 console.error("Error : No rows returned from the quotations table | Result : "+result);
                 $("#topQuoteHomeDiv").html('<blockquote><p>Look up at the stars and not down at your feet. Try to make sense of what you see, and wonder about what makes the universe exist. Be curious. ”</p><footer>- Stephen Hawking</footer></blockquote>');
