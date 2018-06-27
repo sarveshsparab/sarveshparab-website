47a125c28cad034dc29ebe830cc97a5792b27284
Sarvesh Parab <sarveshsparab@gmail.com>
2018-06-28 04:10:33 +0530

    read more data retrieval code

--- php/content/getReadMoreContent.php
+++ php/content/getReadMoreContent.php
@@ -1,9 +1,33 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Sarvesh
- * Date: 26-06-2018
- * Time: 02:26
- */
-echo 'Type : '.$_GET['type'].' | ID : '.$_GET['id'];
+
+include("../db/connectGoDaddyDB.php");
+include("../util/utility.php");
+
+$type = $_GET['type'];
+$id = $_GET['id'];
+
+$table = '';
+
+if ($type == 'edu') {
+    $table = 'education';
+} elseif ($type == 'exp') {
+    $table = 'experience';
+} elseif ($type == 'prj') {
+    $table = 'projects';
+}
+
+$sql = "SELECT * from " . $table . " t WHERE t.id=" . extractID($id);
+$result = $conn->query($sql);
+
+$content = array();
+
+if ($result->num_rows > 0) {
+    $content = $result->fetch_assoc();
+    $content['status'] = 'SUCCESS';
+} else {
+    $content['status'] = 'ERROR';
+}
+
+echo json_encode($content);
+
 ?>
